77020c0391a8fdd759edcc288cf6eb29ab27d325
Maciej Micek <m.micek97@gmail.com>
2025-10-28 11:40:19 +0100

    Add blank to Image model fields

--- file_manager/apps/files/models.py
+++ file_manager/apps/files/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Image(UpdatedCreatedAtModel):
     title = models.CharField(max_length=255, null=False, blank=False, db_index=True, unique=True)
     image = models.ImageField(upload_to="uploads/")
-    width = models.IntegerField()
-    height = models.IntegerField()
+    width = models.IntegerField(blank=True)
+    height = models.IntegerField(blank=True)
 
     def __str__(self):
         return self.title
