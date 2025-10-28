993c39aafccf772252d428f46259e8c2ca3314a3
Maciej Micek <m.micek97@gmail.com>
2025-10-28 11:39:42 +0100

    Add image feature in admin

--- file_manager/apps/files/admin.py
+++ file_manager/apps/files/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from .api.services.image_service import ImageService
 from .models import Image
 
 
@@ -9,3 +10,16 @@ class ImageAdmin(admin.ModelAdmin):
     search_fields = ("title",)
     ordering = ("-updated_at",)
     list_filter = ("updated_at",)
+
+    def save_model(self, request, obj, form, change):
+        width = form.cleaned_data.get("width")
+        height = form.cleaned_data.get("height")
+        image_service = ImageService(obj.image)
+
+        if width and height:
+            obj.image = image_service.resize_image(obj.width, obj.height)
+        else:
+            width, height = image_service.get_image_size()
+
+        obj.width, obj.height = width, height
+        super().save_model(request, obj, form, change)
